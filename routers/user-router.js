181a3c7c9f0717c63a79c25f66ff46c266855d77
cckinwest <kensechan@gmail.com>
2024-05-19 22:38:59 +0100

    add the jwt sign to signup router

--- routers/user-router.js
+++ routers/user-router.js
@@ -7,7 +7,19 @@ const jwt = require("jsonwebtoken");
 router.post("/signup", async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(200).json(user);
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ token });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "internal server error!" });
