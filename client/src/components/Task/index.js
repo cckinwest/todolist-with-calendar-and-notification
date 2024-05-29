5880ad416af88a779e22f01bc8b645c97c2ad188
cckinwest <kensechan@gmail.com>
2024-05-29 21:25:59 +0100

    add edit mode for Task

--- client/src/components/Task/index.js
+++ client/src/components/Task/index.js
@@ -3,8 +3,21 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Task = ({ taskId, title, description }) => {
-  const [tasktitle, setTasktitle] = useState(title);
-  const [taskdescription, setTaskdescription] = useState(description);
+  const [formData, setFormData] = useState({
+    tasktitle: title,
+    taskdescription: description,
+  });
+
+  const [edit, setEdit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
 
   const handleDelete = () => {
     const taskData = { taskId: taskId };
@@ -20,11 +33,34 @@ const Task = ({ taskId, title, description }) => {
       });
   };
 
-  return (
+  return edit ? (
+    <form>
+      <label htmlFor="tasktitle">Title: </label>
+      <input
+        name="tasktitle"
+        id="tasktitle"
+        value={formData.tasktitle}
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="taskdescription">Description: </label>
+      <input
+        name="taskdescription"
+        id="taskdescription"
+        value={formData.taskdescription}
+        onChange={handleChange}
+      />
+      <br />
+      <button type="submit" onClick={handleEdit}>
+        Save
+      </button>
+      <button onClick={handleDelete}>Delete</button>
+    </form>
+  ) : (
     <div style={{ backgroundColor: "yellow", marginBottom: "5px" }}>
-      <h4>Title: {tasktitle}</h4>
-      <p>Description: {taskdescription}</p>
-      <button>Edit</button>
+      <h4>Title: {formData.tasktitle}</h4>
+      <p>Description: {formData.taskdescription}</p>
+      <button onClick={handleEdit}>Edit</button>
       <button onClick={handleDelete}>Delete</button>
     </div>
   );
